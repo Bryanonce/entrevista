7054bc76873b628dd921bff5ba699afba3f10d10
Bryan <bryan.cedeo@gmail.com>
2021-03-03 21:44:29 -0500

    ultimas imagenes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  users: 'https://rolimapp.com:3000/usuarios',
+  products: 'https://rolimapp.com:3000/productos',
+  planes: 'https://rolimapp.com:3000/planes'
 };
